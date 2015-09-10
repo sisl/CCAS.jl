afcc1d5bc03a8c10bc44b5b90a436dec8600f137
Ritchie Lee <ritchie.lee@sv.cmu.edu>
2015-09-09 23:05:58 -0700

    Apply NASA License

--- libccas/src/libccas.cpp
+++ libccas/src/libccas.cpp
@@ -1,3 +1,37 @@
+// *****************************************************************************
+// Written by Ritchie Lee, ritchie.lee@sv.cmu.edu
+// *****************************************************************************
+// Copyright ã 2015, United States Government, as represented by the
+// Administrator of the National Aeronautics and Space Administration. All
+// rights reserved.  The Reinforcement Learning Encounter Simulator (RLES)
+// platform is licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License. You
+// may obtain a copy of the License at
+// http://www.apache.org/licenses/LICENSE-2.0. Unless required by applicable
+// law or agreed to in writing, software distributed under the License is
+// distributed on an "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
+// KIND, either express or implied. See the License for the specific language
+// governing permissions and limitations under the License.
+// _____________________________________________________________________________
+// Reinforcement Learning Encounter Simulator (RLES) includes the following
+// third party software. The SISLES.jl package is licensed under the MIT Expat
+// License: Copyright (c) 2014: Youngjun Kim.
+// Permission is hereby granted, free of charge, to any person obtaining a copy
+// of this software and associated documentation files (the "Software"), to
+// deal in the Software without restriction, including without limitation the
+// rights to use, copy, modify, merge, publish, distribute, sublicense, and/or
+// sell copies of the Software, and to permit persons to whom the Software is
+// furnished to do so, subject to the following conditions:
+// The above copyright notice and this permission notice shall be included in
+// all copies or substantial portions of the Software. THE SOFTWARE IS PROVIDED
+// "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR IMPLIED, INCLUDING BUT
+// NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY, FITNESS FOR A PARTICULAR
+// PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT
+// HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER IN AN
+// ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN
+// CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
+// *****************************************************************************
+
 #include <stdio.h>
 #include <string>
 #include <iostream>
@@ -147,7 +181,7 @@ libccas_EXPORT CIntruderInput getRefCIntruderInput(CCollectionIntruderInput cCol
 	return reinterpret_cast<IntruderInput*>(&((*pCollection)[index]));
 }
 
-libccas_EXPORT void setCIntruderInput(CIntruderInput cIntruderInput, bool valid, uint32 id, 
+libccas_EXPORT void setCIntruderInput(CIntruderInput cIntruderInput, bool valid, uint32 id,
 	uint32 modes, real sr, real chi, real z, uint8 cvc, uint8 vrc, uint8 vsb, int equipage,
 	uint8 quant, uint8 sensitivity_index, uint8 protection_mode)
 {
@@ -258,7 +292,7 @@ libccas_EXPORT CIntruderOutput getRefCIntruderOutput(CCollectionIntruderOutput c
 
 libccas_EXPORT void setCIntrOutput_id(CIntruderOutput cIntruderOutput, uint32 id)
 {
-	IntruderOutput* pIntruderOutput = reinterpret_cast<IntruderOutput*>(cIntruderOutput);	
+	IntruderOutput* pIntruderOutput = reinterpret_cast<IntruderOutput*>(cIntruderOutput);
 	pIntruderOutput->id = id;
 }
 
@@ -550,7 +584,7 @@ libccas_EXPORT const char* version(CCASShared cCASShared)
 {
 	CASShared* pCASShared = reinterpret_cast<CASShared*>(cCASShared);
 	const char* ver = pCASShared->version();
-	
+
 	return ver;
 }
 
@@ -623,7 +657,7 @@ int main(int argc, const char* argv[])
 	if (errorMsg == NULL)
 		myfile << "No errors." << endl;
 	else
-		myfile << "Error: " << errorMsg << endl;	
+		myfile << "Error: " << errorMsg << endl;
 
 	CInput cInput = newCInput();
 
@@ -650,7 +684,7 @@ int main(int argc, const char* argv[])
 		for (int t = 1; t <= 5; t++)
 		{
 			myfile << "t = " << t << endl;
-			
+
 			//set input
 			setCOwnInput(cOwnInput, 0.0, 1665, 0.0, 1665.0, 0x1);
 
