23e9af78b6d5fc15e69266338863bb8642fbc493
Ritchie Lee <ritchie.lee@sv.cmu.edu>
2015-12-11 14:18:07 -0800

    RA active test

--- test/runtests.jl
+++ test/runtests.jl
@@ -1,7 +1,10 @@
 using CASInterface
 using CCAS
 
-function runtest(libcas::String, libcas_config::String)
+const LIBCAS = Pkg.dir("CCAS/libcas0.8.6/lib/libcas")
+const LIBCAS_CONFIG = Pkg.dir("CCAS/libcas0.8.6/parameters/0.8.5.standard.r13.xa.config.txt")
+
+function runtest(libcas::String=LIBCAS, libcas_config::String=LIBCAS_CONFIG)
   println(author())
 
   consts = Constants(25, libcas_config, 1)
@@ -16,6 +19,9 @@ function runtest(libcas::String, libcas_config::String)
   output = Output(nintruders)
   println("output handle = ", cas.output.handle)
 
+  println("===============")
+  println("COC test")
+  println("===============")
   for i=1:5
     println("\ni = ", i)
     reset(cas)
@@ -37,7 +43,7 @@ function runtest(libcas::String, libcas_config::String)
       input.intruders[1].cvc = 0x0
       input.intruders[1].vrc = 0x0
       input.intruders[1].vsb = 0x0
-      input.intruders[1].equipage = EQUIPAGE.EQUIPAGE_ATCRBS
+      input.intruders[1].equipage = EQUIPAGE_MODES
       input.intruders[1].quant = 25
       input.intruders[1].sensitivity_index = 0x0
       input.intruders[1].protection_mode = 0x0
@@ -55,6 +61,47 @@ function runtest(libcas::String, libcas_config::String)
       errorMsg = error_msg(cas)
       println( errorMsg == nothing ? "No Errors" : errorMsg )
     end
+  end
+
+  println("")
+  println("===============")
+  println("active RA test")
+  println("===============")
+  for t = 1:20
+    println("\nt = ", t)
+
+    input.ownInput.dz = 0.0
+    input.ownInput.z = 5000
+    input.ownInput.psi = 0.0
+    input.ownInput.h = 5000
+    input.ownInput.modes = 0x1
+    input.intruders[1].valid = true
+    input.intruders[1].id = 100
+    input.intruders[1].modes = 0x2
+    input.intruders[1].chi = 0.0
+    input.intruders[1].sr = 10000 - 500 * t
+    input.intruders[1].z = 5000
+    input.intruders[1].cvc = 0x0
+    input.intruders[1].vrc = 0x0
+    input.intruders[1].vsb = 0x0
+    input.intruders[1].equipage = EQUIPAGE_TCAS
+    input.intruders[1].quant = 25
+    input.intruders[1].sensitivity_index = 0x0
+    input.intruders[1].protection_mode = 0x0
+
+    update!(cas, input, output)
+
+    #xdump(input.ownInput)
+    #xdump(input.intruders[1])
+    #xdump(output)
+    #xdump(output.intruders[1])
+    println("own_out[",output.cc,",",output.vc,",",output.ua,",",output.da,"]")
+    println("intr_out[",output.intruders[1].cvc,",",output.intruders[1].vrc,",",output.intruders[1].vsb,"]")
+    println("target_rate=",output.target_rate)
+    println("dh_min=",output.dh_min,", dh_max=",output.dh_max)
+
+    errorMsg = error_msg(cas)
+    println( errorMsg == nothing ? "No Errors" : errorMsg )
   end
 
   println()
